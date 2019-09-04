d6a8f2d42a3218c559923b5bb9b9584b689f84ec
h-tatejima <54789265+h-tatejima@users.noreply.github.com>
2019-09-04 17:45:34 +0900

    Add files via upload

--- training/index.py
+++ training/index.py
@@ -5,7 +5,7 @@ from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.exc import NoResultFound
-from flask import Flask
+from flask import Flask, render_template
   
 engine = create_engine('mysql+pymysql://testUser:testPassword@localhost/test')
 
@@ -19,51 +19,51 @@ Base = declarative_base()
 class Employee(Base):
     __tablename__ = 'employee'
  
-    e_name = Cloumn(String)
-    e_name_kana = Cloumn(String)
-    e_name_en = Cloumn(String)
-    postal_code = Cloumn(String)
-    address = Cloumn(String)
-    phone_number = Cloumn(String)
-    email = Cloumn(String)
-    sex = Cloumn(String)
-    birthday = Cloumn(String)
-    final_education = Cloumn(String)
-    join_date = Cloumn(String)
-    company_email = Cloumn(String)
-    e_id = Cloumn(Integer, primary_key=True)
-    image = Cloumn(String)
+    e_name = Column(String(100))
+    e_name_kana = Column(String(100))
+    e_name_en = Column(String(100))
+    postal_code = Column(String(100))
+    address = Column(String(100))
+    phone_number = Column(String(100))
+    email = Column(String(100))
+    sex = Column(String(100))
+    birthday = Column(String(100))
+    final_education = Column(String(100))
+    join_date = Column(String(100))
+    company_email = Column(String(100))
+    e_id = Column(Integer, primary_key=True)
+    image = Column(String(100))
      
     def __repr__(self):
-        return "<Employee(
-        e_name='%s',
-        e_name_kana='%s',
-        e_name_en='%s',
-        postal_code='%s',
-        address='%s',
-        phone_number='%s',
-        email='%s',
-        sex='%s',
-        birthday='%s',
-        final_education='%s',
-        join_date='%s',
-        company_email='%s',
-        e_id='%s',
-        image='%s')>"
-         % (
-        self.e_name,
-        self.e_name_kana,
-        self.e_name_en,
-        self.postal_code,
-        self.address,
-        self.phone_number,
-        self.email,
-        self.sex,
-        self.birthday,
-        self.final_education,
-        self.join_date,
-        self.company_email,
-        self.e_id,
+        return "<Employee(\
+        e_name='%s',\
+        e_name_kana='%s',\
+        e_name_en='%s',\
+        postal_code='%s',\
+        address='%s',\
+        phone_number='%s',\
+        email='%s',\
+        sex='%s',\
+        birthday='%s',\
+        final_education='%s',\
+        join_date='%s',\
+        company_email='%s',\
+        e_id='%s',\
+        image='%s')>"\
+         % (\
+        self.e_name,\
+        self.e_name_kana,\
+        self.e_name_en,\
+        self.postal_code,\
+        self.address,\
+        self.phone_number,\
+        self.email,\
+        self.sex,\
+        self.birthday,\
+        self.final_education,\
+        self.join_date,\
+        self.company_email,\
+        self.e_id,\
         self.image)
  
  
@@ -71,15 +71,24 @@ class Employee(Base):
 # テーブルがない場合 CREATE TABLE 文が実行される
 Base.metadata.create_all(engine)  # 作成した engine を引数にすること
 
+
+app = Flask(__name__)
+
+
+@app.route('/index')
+def  index2():
+    return render_template('/bootstrap/index.html')
+    
+@app.route('/insert')
+def  insert():
+    return render_template('/bootstrap/insert.html')
+
+if __name__ == '__main__':
+    app.run(debug = True, host='0.0.0.0', port=80)
+
 # SQLAlchemy はセッションを介してクエリを実行する
 Session = sessionmaker(bind=engine)
 session = Session()
-
-# 1レコードの追加
-session.add(Student(id=1, name='Suzuki', score=70))
- 
-# コミット（データ追加を実行）
-session.commit()
 
 # セッション・クローズ
 # DB処理が不要になったタイミングやスクリプトの最後で実行
